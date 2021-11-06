6b8224e34e61ebfe0b0104a1ca58f6c1c3f441f2
Abhay <abhaysi100@gmail.com>
2021-11-06 13:30:37 +0530

    Second commit

--- index.js
+++ index.js
@@ -60,7 +60,10 @@ app.post('/aboutme/forms', (req,res)=>{
 const PORT = process.env.PORT || 3001;
 
 if(process.env.NODE_ENV == "production"){
-    app.use(express.static("portfolio/build"))
+    app.use(express.static("portfolio/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname,"portfolio", "build", "index.html"));
+      })
 }
 
 
