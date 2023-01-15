1ce38a7d0abfe03faa42b4506c15fe037d1516fb
Abhay Singh <54500897+Abhay-singh1@users.noreply.github.com>
2023-01-15 15:04:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -63,7 +63,14 @@ const PORT = process.env.PORT || 5000;
 if(process.env.NODE_ENV == "production"){
     app.use(express.static("portfolio/build"));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname,"portfolio/build", "index.html"));
+//         res.sendFile(path.resolve(__dirname,"portfolio/build", "index.html"));
+            res.sendFile(path.join(__dirname, "./frontend/build/index.html"),
+                    function (err) {
+                      if (err) {
+                        res.status(500).send(err);
+                      }
+                    }
+  );
       })
 }
 
